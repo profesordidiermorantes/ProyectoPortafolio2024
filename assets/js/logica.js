0a9a7749e8f0895dd9f9254a2254d2d7c131094f
profesordidiermorantes <profesordidiermorantes@gmail.com>
2024-08-28 19:06:09 -0500

    base para la practica de consolidacion 2

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -1,3 +1,19 @@
+function asignarEventos(){
+    let elBotonForm = document.getElementById("btnForm");
+    elBotonForm.addEventListener('click', mostrarMensaje);
+}
+
+function mostrarMensaje(){
+    // limpiar los campos
+    let elEmail = document.getElementById('emailContacto');
+    elEmail.value = "";
+    let elTelefono = document.getElementById("telefonoContacto");
+    elTelefono.value = "";
+
+    alert('Sus datos fueron registrados');
+
+}
+
 function cambiarColorTitulo(){
     let elTitulo = document.getElementById("tituloEspecial");
     elTitulo.style.color = "green";
